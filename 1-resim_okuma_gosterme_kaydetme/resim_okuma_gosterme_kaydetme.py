a47dc079742189852c1f7f4bb04c9afdfaedeb50
ergunacun <43208505+ergunacun@users.noreply.github.com>
2020-07-03 02:58:17 +0300

    Update resim_okuma_gosterme_kaydetme.py

--- 1-resim_okuma_gosterme_kaydetme/resim_okuma_gosterme_kaydetme.py
+++ 1-resim_okuma_gosterme_kaydetme/resim_okuma_gosterme_kaydetme.py
@@ -1,10 +1,10 @@
 import cv2
 
-img = cv2.imread("E:\klon.jpg",0)
+img = cv2.imread("E:\klon.jpg",0) # 0 değeri gray scale için
 # # print(img)
-cv2.namedWindow("image",cv2.WINDOW_NORMAL)
+cv2.namedWindow("image",cv2.WINDOW_NORMAL) #gösterilen image ölçeklenebilir olması için
 
 cv2.imshow("image",img)
 cv2.imwrite("E:\klon1.jpg",img)
-cv2.waitKey(0)
+cv2.waitKey(0) # image in kapatılana kadar gösterilmesini sağlar
 cv2.destroyAllWindows()
