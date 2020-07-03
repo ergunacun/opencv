f7f9dab47c17faed17dd0d7071929759ba7aaa41
ergunacun <43208505+ergunacun@users.noreply.github.com>
2020-07-03 17:27:52 +0300

    Update video_okuma_izleme.py

--- 2-video_okuma_izleme/video_okuma_izleme.py
+++ 2-video_okuma_izleme/video_okuma_izleme.py
@@ -5,6 +5,7 @@ cap = cv2.VideoCapture("antalya.mp4")
 
 # webcam
 # cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # çıkacak bir hatayı bertaraf eder
 
 while True:
     ret, frame = cap.read()
@@ -16,6 +17,7 @@ while True:
     # frame = cv2.flip(frame, 1)
 
     cv2.imshow("Antalya", frame)
+    # cv2.imshow("Webcam",frame) webcam için
     if cv2.waitKey(10) & 0xFF == ord("q"):
         break
 
